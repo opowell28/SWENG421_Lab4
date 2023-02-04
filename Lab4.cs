cb1dd182d28b19f81f66936885789c571459f658
opowell28 <opowell2899@gmail.com>
2023-02-04 15:52:30 -0500

    bubblesort is fully functioning.

--- Lab4.cs
+++ Lab4.cs
@@ -1,30 +1,53 @@
-public class Company {
-    public Utility<ProductIF> utility;
-}
-
-public class T {
-    private string id;
-    public string name;
-    public double price;
-
-    public T() {
-        this.id = "0";
-        this.name = "generic";
-        this.price = 0.00;
+namespace Lab4 {
+    public interface ProductIF {
+        double Price { get; set; }
+        string Id { get; set; }
+        string Name { get; set; }
     }
-    public T(string id, string name, double price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-}
 
-public class Utility<T> {
-    string sortName { get; set; }
-    public Utility() { this.sortName = "bubblesort"; }
-    public Utility(string sortName) { this.sortName = sortName; }
-    public string getName() { /* simple code */ }
-    public List<T> sort(List<T> data) {
-        
+    public class T : ProductIF {
+        public double Price { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public T() {
+            this.Price = 0.00;
+            this.Id = "000";
+            this.Name = "generic";
+        }
+
+        public T(double price, string id, string name) {
+            this.Price = price;
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+    public class Company {
+        public Utility<ProductIF> utility;
+    }
+
+    public class Utility<T> where T : ProductIF {
+        string sortName { get; set; }
+        public Utility() { this.sortName = "bubblesort"; }
+        public Utility(string sortName) { this.sortName = sortName; }
+        public string getName() { return sortName; }
+        public List<T> sort(List<T> data) {
+            T temp;
+            for (int j = 0; j <= data.Count - 2; j++) {
+                for (int i = 0; i <= data.Count - 2; i++) {
+                    if (data[i].Price > data[i + 1].Price) {
+                        temp = data[i + 1];
+                        data[i + 1] = data[i];
+                        data[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+            foreach (T p in data)
+                Console.Write(p.Id + ", " + p.Name + ", " + p.Price + " | ");
+            Console.Read();
+            return data;
+        }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Lab4;
+
+public class Program {
+    static void Main(string[] args) {
+        T t1 = new T(12.25, "001", "One");
+        T t2 = new T(31.32, "002", "Two");
+        T t3 = new T(13.19, "003", "Three");
+
+        List<T> list = new List<T>();
+
+        list.Add(t1);
+        list.Add(t2);
+        list.Add(t3);
+
+        Utility<T> util = new Utility<T>();
+        util.sort(list);
+    }
+}
